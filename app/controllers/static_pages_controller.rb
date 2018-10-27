d0ce267dbafc01d43b9b8db1a03b217aee0ed045
Aristotle Poultney <aristotlepoultney@gmail.com>
2018-10-26 20:23:56 -0400

    Finished unpolished api

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,15 @@
 class StaticPagesController < ApplicationController
   def index
+    if !params[:search].blank?
+      user_id = params[:search][:user]
+    else
+      user_id = "157309799@N04"
+    end
+    @results = flickr.people.getPhotos(user_id: user_id, extras: "url_m", per_page: 50)
   end
+
+  private
+    def api_params
+      params.require(:search).permit(:user)
+    end
 end
---
+++ config/initializers/flickraw.rb
@@ -0,0 +1,2 @@
+FlickRaw.api_key = Figaro.env.flickraw_api_key
+FlickRaw.shared_secret = Figaro.env.flickraw_shared_secret
